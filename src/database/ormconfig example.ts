c0d85fe4e07c0eea53f13a5fce61734fdbc51994
daniphant <daniphnt_dev@yahoo.com>
2021-02-25 21:27:29 -0300

    add different db's for each env

--- src/database/ormconfig example.ts
+++ src/database/ormconfig example.ts
@@ -2,9 +2,6 @@ import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOpti
 
 const connectionOptions: MysqlConnectionOptions = {
   type: "mysql",
-  username: "root",
-  password: "",
-  database: "nodenps",
   entities: [`${__dirname}/../models/*.ts`],
   migrations: [`${__dirname}/migrations/*.ts`],
   cli: {
@@ -12,4 +9,24 @@ const connectionOptions: MysqlConnectionOptions = {
   },
 };
 
-export default connectionOptions;
+// extends connectionOptions with data based on which environment you're on
+export default {
+  test: {
+    ...connectionOptions,
+    username: "",
+    password: "",
+    database: "",
+  },
+  dev: {
+    ...connectionOptions,
+    username: "",
+    password: "",
+    database: "",
+  },
+  prod: {
+    ...connectionOptions,
+    username: "",
+    password: "",
+    database: "",
+  },
+};
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import app from "./app";
 import connectionOptions from "./database/ormconfig";
 
 const main = async () => {
-  await createConnection(connectionOptions);
+  await createConnection(connectionOptions[process.env.NODE_ENV]);
 
   app.listen(3333, () => console.log("Server is up."));
 };
